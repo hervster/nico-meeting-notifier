3b43e317d6b43807ef8279e91a42f1702e6424cc
hervster <hervenyemeck@gmail.com>
2022-04-04 15:03:08 -0400

    Starting over actually

---
+++ backend/index.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const cors = require("cors");
+
+// DB setup
+
+mongoose.connect('mongodb://localhost:27017/', {
+    dbName: 'NicoMeeting',
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, err => err ? console.log(err) : 
+    console.log('Connected to NicoMeeting database'));
+
+const MeetingStateSchema = new mongoose.Schema({
+    meetState: { 
+        type: Boolean,
+        required: true,
+        }
+    });
+
+MeetingStateSchema.methods.getState = function getState() {
+    return this.meetState;
+}
+
+MeetingStateSchema.methods.setState = function setState(input) {
+    this.meetState = input;
+}
+
+const meetingState = mongoose.model("inMeeting", MeetingStateSchema);
+meetingState.createIndexes();
+
+// Express
+const app = express();
+console.log("App listening at port 5000");
+app.use(express.json());
+app.use(cors());
+app.get("/", (req, res) => {
+    res.send("App is Working");
+
+})
+
+app.get("/getState", async(res) => {
+    let result = getMeetingState();
+    res.send("State is: " + result)
+    return result;
+})
+
+app.post("/saveState", async (req, res) => {
+    setMeetingState(req.body.meetState);
+    console.log("State set")
+})
+
+const getMeetingState = async () => {
+    const newMeetState = await meetingState.find();
+    const s = newMeetState.getState();
+    console.log("Meeting state is: " + s);
+    return s;
+}
+
+const setMeetingState = async (stateInput) => {
+    const newMeetState = await meetingState.find();
+    newMeetState.setState(stateInput);
+    newMeetState.save();
+} 
